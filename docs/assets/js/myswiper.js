3eab5242859bf61871b754043142f2770ae5b120
SaeedMawed <saeed.moued@gmail.com>
2022-10-17 20:33:11 +0300

    change base url

---
+++ docs/assets/js/myswiper.js
@@ -0,0 +1,44 @@
+
+export var suggestions_swiper = new Swiper('.offers-swiper-container', {
+  slidesPerView: 3,
+  //spaceBetween: 20,
+  loop: true,
+  watchOverflow: false,
+  //centeredSlides: true,
+  pagination: {
+      //el: '.swiper-pagination',
+      //clickable: true,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+      20: {
+          slidesPerView: 1,
+          spaceBetween: 20
+      },
+      360: {
+          slidesPerView: 2,
+          spaceBetween: 15
+      },
+      650: {
+          slidesPerView: 3,
+          spaceBetween: 15
+      },
+      850: {
+          slidesPerView: 4,
+          spaceBetween: 15
+      },
+      1000: {
+          slidesPerView: 5,
+          spaceBetween: 15
+      }
+
+  },
+  autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+  },
+});
