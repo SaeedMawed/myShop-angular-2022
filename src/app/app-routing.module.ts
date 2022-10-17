ccedf11e2828a9011a9fa069c39807f8a379e04b
SaeedMawed <saeed.moued@gmail.com>
2022-10-17 21:17:16 +0300

    deply project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { MycartComponent } from './pages/mycart/mycart.component';
 
 const routes: Routes = [
-  {path:"",redirectTo:"/home",pathMatch:"full"},
+  {path:"",redirectTo:"home",pathMatch:"full"},
   {path:"home",component:HomeComponent},
   {path:"cart",component:MycartComponent}
 ];
